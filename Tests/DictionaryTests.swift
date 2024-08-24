c661f18124b95eb9a7b0fee584b8bf4b93dae8c7
Jessy Catterwaul <jessy@catterwaul.com>
2024-08-23 20:50:33 -0400

    Remove OrderedDictionary

--- Tests/DictionaryTests.swift
+++ Tests/DictionaryTests.swift
@@ -1,35 +1,27 @@
-import typealias Collections.OrderedDictionary
-import Algorithms
-import HMAlgorithms
 import Testing
 
 struct DictionaryTests {
   @Test func init_grouping_KeyValuePairs() {
-    func test<Dictionary: DictionaryProtocol<String, [String]> & Equatable>(_: Dictionary.Type) {
-      let dictionary = [
-        "🔑": [ "🐅", "🐆", "🐈"],
-        "🗝": ["🦖", "🦕"]
-      ] as Dictionary
-
-      #expect(
-        Dictionary(
-          grouping: [
-            ("🔑", "🐅"), ("🔑", "🐆"), ("🔑", "🐈"),
-            ("🗝", "🦖"), ("🗝", "🦕")
-          ]
-        ) == dictionary
-      )
-
-      #expect(
-        Dictionary(
-          grouping: [
-            "🔑": "🐅", "🔑": "🐆", "🔑": "🐈",
-            "🗝": "🦖", "🗝": "🦕"
-          ] as KeyValuePairs
-        ) == dictionary
-      )
+    let keyValuePairs = [("🔑", "🐅"), ("🗝", "🦖")]
+    _ = Dictionary(grouping: keyValuePairs) // No crash.
+    func test<Dictionary: DictionaryProtocol<String, [String]>>(_: Dictionary.Type) {
+      _ = Dictionary(grouping: keyValuePairs) // EXC_BAD_ACCESS
     }
     test(Dictionary.self)
-    test(OrderedDictionary.self)
+  }
+}
+
+protocol DictionaryProtocol<Key, Value> {
+  associatedtype Key
+  associatedtype Value
+
+  init<ValueElement>(grouping pairs: some Sequence<(Key, ValueElement)>)
+  where Value == [ValueElement]
+}
+
+extension Dictionary: DictionaryProtocol {
+  init<ValueElement>(grouping pairs: some Sequence<(Key, ValueElement)>)
+  where Value == [ValueElement] {
+    self = [_: _](grouping: pairs, by: \.0).mapValues { $0.map(\.1) }
   }
 }

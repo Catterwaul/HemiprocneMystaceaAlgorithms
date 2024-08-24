ccdeba4e2b6f2343423e57c6e67cdcea429340eb
Jessy Catterwaul <jessy@catterwaul.com>
2024-08-23 21:23:04 -0400

    simplify

--- Tests/DictionaryTests.swift
+++ Tests/DictionaryTests.swift
@@ -1,27 +1,24 @@
 import Testing
 
-struct DictionaryTests {
-  @Test func init_grouping_KeyValuePairs() {
-    let keyValuePairs = [("🔑", "🐅"), ("🗝", "🦖")]
-    _ = Dictionary(grouping: keyValuePairs) // No crash.
-    func test<Dictionary: DictionaryProtocol<String, [String]>>(_: Dictionary.Type) {
-      _ = Dictionary(grouping: keyValuePairs) // EXC_BAD_ACCESS
-    }
-    test(Dictionary.self)
+@Test func bug() {
+  _ = Struct(1) // No crash.
+
+  func test<P: Protocol<[Int]>>(_: P.Type) {
+    _ = P(1) // EXC_BAD_ACCESS
   }
+  test(Struct.self)
 }
 
-protocol DictionaryProtocol<Key, Value> {
-  associatedtype Key
+protocol Protocol<Value> {
   associatedtype Value
 
-  init<ValueElement>(grouping pairs: some Sequence<(Key, ValueElement)>)
-  where Value == [ValueElement]
+  init<ValueElement>(_: ValueElement) where Value == [ValueElement]
 }
 
-extension Dictionary: DictionaryProtocol {
-  init<ValueElement>(grouping pairs: some Sequence<(Key, ValueElement)>)
-  where Value == [ValueElement] {
-    self = [_: _](grouping: pairs, by: \.0).mapValues { $0.map(\.1) }
+struct Struct<Value>: Protocol {
+  init() { }
+
+  init<ValueElement>(_: ValueElement) where Value == [ValueElement] {
+    self.init()
   }
 }

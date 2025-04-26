4aa48b877b766b96b6e8308be59954c65782c7e9
Jessy Catterwaul <jessy@catterwaul.com>
2025-04-26 11:08:06 -0400

    Move `zip` overload from Thrappture

--- Tests/Standard Library/Collection/Lazy Views/ZipTests.swift
+++ Tests/Standard Library/Collection/Lazy Views/ZipTests.swift
@@ -2,10 +2,35 @@ import HMAlgorithms
 import Testing
 
 struct ZipTests {
-  @Test func _zip() {
+  @Test func constant() {
     #expect(
       zip(1...3, constant: "🧛🏼")
         .elementsEqual([(1, "🧛🏼"), (2, "🧛🏼"), (3, "🧛🏼")], by: ==)
+    )
+  }
+
+  @Test func pack() {
+    let sequences = (
+      1...5,
+      ["🇨🇦", "🐝", "🌊"],
+      stride(from: 20, through: 80, by: 20),
+      AnyIterator { "😺" }
+    )
+
+    let zipped = zip(
+      sequences.0,
+      sequences.1,
+      sequences.2,
+      sequences.3
+    )
+    #expect(
+      zipped.elementsEqual(
+        [ (1, "🇨🇦", 20, "😺"),
+          (2, "🐝", 40, "😺"),
+          (3, "🌊", 60, "😺")
+        ],
+        by: ==
+      )
     )
   }
 }

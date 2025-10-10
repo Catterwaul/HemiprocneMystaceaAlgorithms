ff59e3b001b6bc8dadfecb086172e3d3e7ea4dbd
Jessy Catterwaul <jessy@catterwaul.com>
2025-10-10 03:00:45 -0400

    Update package manifest

--- Package.swift
+++ Package.swift
@@ -1,73 +1,177 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 
 import Foundation // for String.capitalized
 import PackageDescription
 
-let shortenedName = "HMAlgorithms"
-
 _ = Package(
   name: "HemiprocneMystaceaAlgorithms",
-  platforms: [.iOS(.v13), .macOS(.v10_15)],
-  products: [.library(name: shortenedName, targets: [shortenedName])],
-  dependencies: dependencies.map(\.package),
-  targets: [
-    .target(
-      name: shortenedName,
-      dependencies: dependencies.dropFirst().map(\.product)
-    ),
-    .testTarget(
-      name: shortenedName + ".Tests",
-      dependencies: [.init(stringLiteral: shortenedName)]
+  platforms: [.iOS(.v26), .macOS(.v26)],
+  products: [
+    .library(
+      name: .LibraryName.unsuffixed,
+      targets: [.LibraryName.unsuffixed]
     )
-  ]
+  ],
+  dependencies:
+    [Module.ExternalDependency.Swift.docC.package] // Necessary for documentation
+    + Set(modules.flatMap(\.externalDependencies)).map(\.package),
+  targets: modules.map(\.target)
 )
 
-// MARK: - Dependency
-
-nonisolated var dependencies: [Dependency]  {
-  [ .swift(repositoryName: "docc-plugin"),
-    .apple(repositoryName: "algorithms"),
-    .apple(repositoryName: "collections"),
-    .catterwaul(name: "Cast"),
-    .catterwaul(name: "Thrappture"),
-    .catterwaul(name: "Tuplé", repositoryName: "Tuplay")
+/// The modules that make up this package.
+var modules: [Module] {
+  [ .init(
+      target: .target(name: .LibraryName.unsuffixed, path: "Sources"),
+      externalDependencies: [
+        .Swift.algorithms,
+        .Swift.collections,
+        .Catterwaul.cast,
+        .Catterwaul.hmError,
+        .Catterwaul.thrappture,
+        .Catterwaul.tuplé
+      ]
+    ),
+    .init(
+      target: .testTarget(
+        name: .LibraryName.suffixed("Tests")
+      ),
+      internalDependencyNames: [.LibraryName.unsuffixed]
+    )
   ]
 }
 
-struct Dependency {
-  let package: Package.Dependency
-  let product: Target.Dependency
+extension String {
+  enum LibraryName {
+    static let unsuffixed = "HMAlgorithms"
+    static func suffixed(_ suffix: String) -> String {
+      "\(unsuffixed).\(suffix)"
+    }
+  }
 }
 
-extension Dependency {
-  static func apple(repositoryName: String) -> Self {
-    .swift(organization: "apple", repositoryName: repositoryName)
+// MARK: - Module
+struct Module {
+  let target: Target
+  let externalDependencies: [ExternalDependency]
+
+  /// - Parameters:
+  ///   - internalDependencyNames: Names of targets within this package.
+  init(
+    target: Target,
+    internalDependencyNames: [String] = [],
+    externalDependencies: [ExternalDependency] = []
+  ) {
+    target.dependencies += internalDependencyNames.map(Target.Dependency.init)
+    target.dependencies += externalDependencies.flatMap(\.products)
+    self.target = target
+    self.externalDependencies = externalDependencies
+  }
+}
+
+extension Module {
+  /// A repository on GitHub.
+  struct ExternalDependency {
+    let organization: String
+    let repositoryName: String
+    let packageNames: [String]
+    let branch: String
+  }
+}
+
+// MARK: - Module.ExternalDependency
+extension Module.ExternalDependency {
+  // MARK: - commonly-used
+  enum Swift {
+    static var algorithms: Module.ExternalDependency { apple(repositoryName: "algorithms") }
+    static var asyncAlgorithms: Module.ExternalDependency { apple(repositoryName: "async-algorithms") }
+    static var macros: Module.ExternalDependency {
+      dependency(repositoryName: "syntax", packageNames: ["SyntaxMacros", "CompilerPlugin"])
+    }
+    static var numerics: Module.ExternalDependency { dependency(repositoryName: "numerics") }
+    static var collections: Module.ExternalDependency { apple(repositoryName: "collections") }
+    static var docC: Module.ExternalDependency { dependency(repositoryName: "docc-plugin") }
+
+    private static func apple(repositoryName: String) -> Module.ExternalDependency {
+      dependency(organization: "apple", repositoryName: repositoryName)
+    }
+
+    private static func dependency(
+      organization: String = "swiftlang", repositoryName: String,
+      packageNames: [String]? = nil
+    ) -> Module.ExternalDependency {
+      .init(
+        organization: organization,
+        repositoryName: "swift-\(repositoryName)",
+        packageNames:
+          packageNames.map { names in names.map { "Swift\($0)" } }
+          ?? [repositoryName.split(separator: "-").map(\.capitalized).joined()]
+      )
+    }
   }
 
-  static func swift(organization: String = "swiftlang", repositoryName: String) -> Self {
-    .init(
+  enum Catterwaul {
+    static var and: Module.ExternalDependency { dependency(name: "And") }
+    static var cast: Module.ExternalDependency { dependency(name: "Cast") }
+    static var hmAlgorithms: Module.ExternalDependency { hm("Algorithms") }
+    static var hmError: Module.ExternalDependency { hm("Error") }
+    static var hmNumerics: Module.ExternalDependency { hm("Numerics") }
+    static var littleAny: Module.ExternalDependency { dependency(name: "LittleAny") }
+    static var thrappture: Module.ExternalDependency { dependency(name: "Thrappture") }
+    static var tuplé: Module.ExternalDependency { dependency(name: "Tuplé", repositoryName: "Tuplay") }
+
+    private static func hm(_ name: String) -> Module.ExternalDependency {
+      dependency(
+        name: "HM\(name)",
+        repositoryName: "HemiprocneMystacea\(name)"
+      )
+    }
+
+    private static func dependency(
+      name: String, repositoryName: String? = nil, branch: String? = nil
+    ) -> Module.ExternalDependency {
+      .init(
+        organization: "Catterwaul",
+        repositoryName: repositoryName ?? name,
+        packageNames: [name],
+        branch: branch
+      )
+    }
+  }
+
+  private init(organization: String, repositoryName: String, packageNames: [String], branch: String? = nil) {
+    self.init(
       organization: organization,
-      name: repositoryName.split(separator: "-").map(\.capitalized).joined(),
-      repositoryName: "swift-\(repositoryName)"
+      repositoryName: repositoryName,
+      packageNames: packageNames,
+      branch: branch ?? "main"
     )
   }
 
-  static func catterwaul(name: String, repositoryName: String? = nil, branch: String? = nil) -> Self {
-    .init(
-      organization: "Catterwaul",
-      name: name,
-      repositoryName: repositoryName ?? name,
+  // MARK: - instance members
+  var package: Package.Dependency {
+    .package(
+      url: "https://github.com/\(organization)/\(repositoryName)",
       branch: branch
     )
   }
 
-  private init(organization: String, name: String, repositoryName: String, branch: String? = nil) {
-    self.init(
-      package: .package(
-        url: "https://github.com/\(organization)/\(repositoryName)",
-        branch: branch ?? "main"
-      ),
-      product: .product(name: name, package: repositoryName)
-    )
+  var products: [Target.Dependency] {
+    packageNames.map { .product(name: $0, package: repositoryName) }
+  }
+}
+
+// MARK: Equatable
+extension Module.ExternalDependency: Equatable {
+  static func == (dependency0: Self, dependency1: Self) -> Bool {
+    (dependency0.organization, dependency0.repositoryName) ==
+    (dependency1.organization, dependency1.repositoryName)
+  }
+}
+
+// MARK: Hashable
+extension Module.ExternalDependency: Hashable {
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(organization)
+    hasher.combine(repositoryName)
   }
 }
